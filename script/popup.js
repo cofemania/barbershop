8ddf75287def7f2dbfc9a7193217d89e39db4163
cofemania <dovahkiin598@gmail.com>
2017-10-25 12:54:39 +0800

    Убрал лишний скрипт

--- script/popup.js
+++ script/popup.js
@@ -6,6 +6,10 @@ var login = popup.querySelector("[name=login]");
 var password = popup.querySelector("[name=password");
 var form = popup.querySelector("form");
 var storage = localStorage.getItem("login");
+var mapLink = document.querySelector(".adress");
+var mapPopup = document.querySelector(".modal-content-map");
+var mapClose = document.querySelector(".map-content-close");
+var mapShadow = document.querySelector(".modal-overlay");
 
 link.addEventListener("click", function(event) {
     event.preventDefault();
@@ -43,6 +47,29 @@ window.addEventListener("keydown", function(event) {
             popup.classList.remove("modal-content-show");
             shadow.classList.remove("modal-overlay-show");
             popup.classList.remove("modal-error");
+        };
+    }
+});
+mapLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    mapPopup.classList.add("map-show");
+    mapShadow.classList.add("modal-overlay-show");
+});
+mapClose.addEventListener("click", function(event){
+    event.preventDefault();
+    mapPopup.classList.remove("map-show");
+    mapShadow.classList.remove("modal-overlay-show");
+});
+mapShadow.addEventListener("click", function(event) {
+    event.preventDefault();
+    mapPopup.classList.remove("map-show");
+    mapShadow.classList.remove("modal-overlay-show");
+});
+window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (mapPopup.classList.contains("map-show")) {
+            mapPopup.classList.remove("map-show");
+            mapShadow.classList.remove("modal-overlay-show");
         };
     }
 });
